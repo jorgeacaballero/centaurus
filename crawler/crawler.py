0ed99cd1dfa97a956fb4c623abb420602363858d
Jorge Caballero <j@caballeromurillo.com>
2017-05-13 07:53:16 -0600

    validating other errors

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -55,69 +55,73 @@ if len(sys.argv) > 4:
     delay = int(sys.argv[5])
 
 for index in range(start, ends):
-    time.sleep(delay)
-
-    url = "https://arxiv.org/abs/%s%s.%s" % (str(year),str(month).zfill(2), str(index).zfill(5))
     while True:
         try:
-            site = urlopen(url)
-            conn = connect()
-            cur = conn.cursor()
-            break
-        except urllib2.HTTPError as e:
-            print "!!! BLOCKED !!!\nFix connection and run: python crawler.py %s %s %s %s %s" % (str(year),str(month),index, ends, delay)
-            print('\a')
-            time.sleep(60)
-    if site.getcode() >= 200 and site.getcode() <= 400:
-        page = site.read()
-        soup = BeautifulSoup(page, "html5lib")
+            time.sleep(delay)
 
-        title = soup.select("h1.title.mathjax")[0].getText()
-        title = title.split("Title:\n")[1]
-
-        authors_text = soup.find_all("div", class_="authors")[0].find_all("a")
-        authors = []
-        authors_id = []
-        for au in authors_text:
-            author_name = au.getText()
-            authors.append(au.getText())
-            cur.execute('select id from author where name like ?', au.getText())
-            rows = cur.fetchall()
-            if len(rows) > 0:
-                for row in rows:
-                    authors_id.append(row.id)
-            else:
-                cur.execute('insert into author (name) output Inserted.id values (?)', author_name)
-                rows = cur.fetchall()
-                if len(rows) > 0:
-                    for row in rows:
-                        authors_id.append(row.id)
-        authors_string = ",".join(authors)
-
-        abstract = soup.select("blockquote.abstract.mathjax")[0].getText()
-        abstract = abstract.split("Abstract: ")[1]
-        data_id = None
-        try:
-            cur.execute('insert into data (title, authors,abstract,url, year, month) output Inserted.id values (?,?,?,?,?,?)', title, authors_string, abstract, url, str(year), str(month))
-            row = cur.fetchone()
-            if row:
-                data_id = row.id
-            else:
-                data_id = None
-            print "[NEW %s] %s -> %s" % (data_id, url, title)
-        except pyodbc.IntegrityError as e:
-            print "[EXISTING] %s -> %s" % (url, title)
-
-        for author_id in authors_id:
-            if data_id is not None:
+            url = "https://arxiv.org/abs/%s%s.%s" % (str(year),str(month).zfill(2), str(index).zfill(5))
+            while True:
                 try:
-                    cur.execute('insert into author_data (data_id, author_id) values (?,?)', data_id, author_id)
-                except pyodbc.IntegrityError as e:
-                    print "[ERROR] REPEATING AUTHOR"
-        conn.commit()
-        conn.close()
-    else:
-        print "[ERROR] %s -> Got Error [%s]" % (url, site.getcode())
+                    site = urlopen(url)
+                    conn = connect()
+                    cur = conn.cursor()
+                    break
+                except urllib2.HTTPError as e:
+                    print "!!! BLOCKED !!!\nFix connection and run: python crawler.py %s %s %s %s %s" % (str(year),str(month),index, ends, delay)
+                    print('\a')
+                    time.sleep(60)
+            if site.getcode() >= 200 and site.getcode() <= 400:
+                page = site.read()
+                soup = BeautifulSoup(page, "html5lib")
 
+                title = soup.select("h1.title.mathjax")[0].getText()
+                title = title.split("Title:\n")[1]
+
+                authors_text = soup.find_all("div", class_="authors")[0].find_all("a")
+                authors = []
+                authors_id = []
+                for au in authors_text:
+                    author_name = au.getText()
+                    authors.append(au.getText())
+                    cur.execute('select id from author where name like ?', au.getText())
+                    rows = cur.fetchall()
+                    if len(rows) > 0:
+                        for row in rows:
+                            authors_id.append(row.id)
+                    else:
+                        cur.execute('insert into author (name) output Inserted.id values (?)', author_name)
+                        rows = cur.fetchall()
+                        if len(rows) > 0:
+                            for row in rows:
+                                authors_id.append(row.id)
+                authors_string = ",".join(authors)
+
+                abstract = soup.select("blockquote.abstract.mathjax")[0].getText()
+                abstract = abstract.split("Abstract: ")[1]
+                data_id = None
+                try:
+                    cur.execute('insert into data (title, authors,abstract,url, year, month) output Inserted.id values (?,?,?,?,?,?)', title, authors_string, abstract, url, str(year), str(month))
+                    row = cur.fetchone()
+                    if row:
+                        data_id = row.id
+                    else:
+                        data_id = None
+                    print "[NEW %s] %s -> %s" % (data_id, url, title)
+                except pyodbc.IntegrityError as e:
+                    print "[EXISTING] %s -> %s" % (url, title)
+
+                for author_id in authors_id:
+                    if data_id is not None:
+                        try:
+                            cur.execute('insert into author_data (data_id, author_id) values (?,?)', data_id, author_id)
+                        except pyodbc.IntegrityError as e:
+                            print "[ERROR] REPEATING AUTHOR"
+                conn.commit()
+                conn.close()
+            else:
+                print "[ERROR] %s -> Got Error [%s]" % (url, site.getcode())
+            break
+        except Exception as e:
+            print e
 print "Bye!"
 
